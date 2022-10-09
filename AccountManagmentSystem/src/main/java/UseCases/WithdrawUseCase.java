6a856a472b766e2589384c8b7d731f88ee22a560
Aman103767 <avinash103767@gmail.com>
2022-10-09 18:01:02 +0530

    files

--- AccountManagmentSystem/src/main/java/UseCases/WithdrawUseCase.java
+++ AccountManagmentSystem/src/main/java/UseCases/WithdrawUseCase.java
@@ -1,12 +1,7 @@
 package UseCases;
 
-import java.util.Scanner;
-
 public class WithdrawUseCase {
 public static void main(String[] args) {
-	 Scanner input = new Scanner(System.in);
-	 System.out.println("Enter the amount");
-	 int amount = input.nextInt();
-	 System.out.println("Enter the account number");
+	
 }
 }
