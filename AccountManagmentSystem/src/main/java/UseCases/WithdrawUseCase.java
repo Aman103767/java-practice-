986f5eab9107fea11afde49d2fdc92939125c59b
Aman103767 <avinash103767@gmail.com>
2022-10-09 18:04:43 +0530

    files

--- AccountManagmentSystem/src/main/java/UseCases/WithdrawUseCase.java
+++ AccountManagmentSystem/src/main/java/UseCases/WithdrawUseCase.java
@@ -2,6 +2,9 @@ package UseCases;
 
 import java.util.Scanner;
 
+import com.masai.dao.AccountDao;
+import com.masai.dao.AccountDaoImpl;
+
 public class WithdrawUseCase {
 public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
@@ -9,6 +12,8 @@ public static void main(String[] args) {
 	int amount = input.nextInt();
 	System.out.println("Enter the account id ");
 	int id = input.nextInt();
+	AccountDao ad = new AccountDaoImpl();
+	System.out.println(ad.withdraw(amount, id));
 	
 
 }
