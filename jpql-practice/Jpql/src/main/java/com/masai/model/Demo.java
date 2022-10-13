58ac7a5f3469fdb54ff125cbba688c67423dd3b9
Aman103767 <avinash103767@gmail.com>
2022-10-13 06:33:22 +0530

    jpql

--- jpql-practice/Jpql/src/main/java/com/masai/model/Demo.java
+++ jpql-practice/Jpql/src/main/java/com/masai/model/Demo.java
@@ -8,11 +8,13 @@ import com.masai.Utility.Utility;
 public class Demo {
 public static void main(String[] args) {
 	EntityManager em = Utility.provideConnection();
-    String jpql = "update Account set balance = balance+100";
+    String jpql = "update Account set balance = balance+?1 where name = ?2";
     Query q = em.createQuery(jpql);
+    q.setParameter(1,1000);
+    q.setParameter(2,"aman");
     em.getTransaction().begin();
     int x = q.executeUpdate();
     em.getTransaction().commit();
-    System.out.println(x + "rows updated");
+    System.out.println(x + " rows updated");
 }
 }
