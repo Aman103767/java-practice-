a9d8904d2ad6bf3158b43e66204bc16f5c2243c0
Aman103767 <avinash103767@gmail.com>
2022-10-27 18:37:36 +0530

    pratice

---
+++ sprint-3/question1/src/main/java/com/masai/bean/Employee.java
@@ -0,0 +1,42 @@
+package com.masai.bean;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private String empAddress;
+	private int salary;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empAddress=" + empAddress + ", salary=" + salary
+				+ "]";
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpAddress() {
+		return empAddress;
+	}
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+	
+}
