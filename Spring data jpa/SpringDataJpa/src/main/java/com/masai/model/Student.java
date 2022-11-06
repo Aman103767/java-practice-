870944da3f76249e5ade67e835149df2bd2dee59
Aman103767 <avinash103767@gmail.com>
2022-11-06 19:48:22 +0530

    springdatajpa

---
+++ Spring data jpa/SpringDataJpa/src/main/java/com/masai/model/Student.java
@@ -0,0 +1,40 @@
+package com.masai.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer roll;
+	private String name ;
+	private int marks;
+	public Integer getRoll() {
+		return roll;
+	}
+	public void setRoll(Integer roll) {
+		this.roll = roll;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	@Override
+	public String toString() {
+		return "Student [roll=" + roll + ", name=" + name + ", marks=" + marks + "]";
+	}
+	
+
+}
