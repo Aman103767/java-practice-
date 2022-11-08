476959dc500e439602af31a6e0514ca931a4e537
Aman103767 <avinash103767@gmail.com>
2022-11-08 21:23:47 +0530

    practice

---
+++ relationship with spring data jpa/springBootRelationShipApp/src/main/java/com/masai/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.masai.service;
+
+import com.masai.model.Student;
+
+public class StudentService {
+
+	
+public Student registerStudentInCourse(String cname, Student student) throws StudentException{
+		
+	}
+}
